d2d5eaf446a0d47819456abec2f7313eed31df08
ArdaCimen <cimenarda.yaz@gmail.com>
2024-10-17 15:34:56 +0300

    Referanslar,Hizmetler, Araba,Adres Listesi Ve ContactUs İşlemi

---
+++ Core/UdemyCarBook.Application/Features/MediatR/Queries/TestimonialQueries/GetTestimonialQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyCarBook.Application.Features.MediatR.Results.TestimonialResults;
+
+namespace UdemyCarBook.Application.Features.MediatR.Queries.TestimonialQueries
+{
+    public class GetTestimonialQuery :IRequest<List<GetTestimonialQueryResult>>
+    {
+    }
+}
---
+++ Frontends/UdemyCarBook.WebUI/ViewComponents/ServicesViewComponents/_OurServicesComponentPartial.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using UdemyCarBook.Dto.ServicesDtos;
+using UdemyCarBook.Dto.TestimonialDtos;
+
+namespace UdemyCarBook.WebUI.ViewComponents.ServicesViewComponents
+{
+    public class _OurServicesComponentPartial :ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _OurServicesComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7005/api/Services");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultServicesDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
