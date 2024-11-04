cf6472ab2fb31082e081c8cc60dd17a5086e425e
ArdaCimen <cimenarda.yaz@gmail.com>
2024-11-04 14:58:06 +0300

    Pivot Tablo=>Araç-Ödeme Paketleri

---
+++ Core/UdemyCarBook.Application/Features/MediatR/Queries/CarPricingQueries/GetCarPricingWithTimePeriodQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyCarBook.Application.Features.MediatR.Results.CarPricingResults;
+
+namespace UdemyCarBook.Application.Features.MediatR.Queries.CarPricingQueries
+{
+	public class GetCarPricingWithTimePeriodQuery :IRequest<List<GetCarPricingWithTimePeriodQueryResult>>
+	{
+
+	}
+}
--- Infrastructure/Udemy.CarBook.Persistance/Repositories/CarPricingRepositories/CarPricingRepository.cs
+++ Infrastructure/Udemy.CarBook.Persistance/Repositories/CarPricingRepositories/CarPricingRepository.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Udemy.CarBook.Persistance.Context;
 using UdemyCarBook.Application.Interfaces.CarPricingInterfaces;
+using UdemyCarBook.Application.ViewModels;
 using UdemyCarBook.Domain.Entities;
 
 namespace Udemy.CarBook.Persistance.Repositories.CarPricingRepositories
@@ -24,6 +25,38 @@ namespace Udemy.CarBook.Persistance.Repositories.CarPricingRepositories
 			var values = _context.CarPricings.Include(x => x.Car).ThenInclude(y => y.Brand).Include(x => x.Pricing).Where(q=>q.PricingID==2).ToList();
 			//CarpricingId si 2 olanları yani pricing günlük olanları listeliycek böylelikle car sayfasında(Car/Index) ödeme kısmı günlük fiyatlardan listelenecek
 			return values;
+		}
+
+		public List<CarPricingViewModel> GetCarPricingWithTimePeriod()
+		{
+			List<CarPricingViewModel> values = new List<CarPricingViewModel>();
+			using (var command = _context.Database.GetDbConnection().CreateCommand())
+			{
+				command.CommandText = "Select * From (Select Model,Name,CoverImageUrl,PricingID,Amount From CarPricings Inner Join Cars On Cars.CarID=CarPricings.CarId Inner Join Brands On Brands.BrandID=Cars.BrandID) As SourceTable Pivot (Sum(Amount) For PricingID In ([3],[4],[8])) as PivotTable;";
+				command.CommandType = System.Data.CommandType.Text;
+				_context.Database.OpenConnection();
+				using (var reader = command.ExecuteReader())
+				{
+					while (reader.Read())
+					{
+						CarPricingViewModel carPricingViewModel = new CarPricingViewModel()
+						{
+							Brand = reader["Name"].ToString(),
+							Model = reader["Model"].ToString(),
+							CoverImageUrl = reader["CoverImageUrl"].ToString(),
+							Amounts = new List<decimal>
+							{
+								Convert.ToDecimal(reader["3"]),
+								Convert.ToDecimal(reader["4"]),
+								Convert.ToDecimal(reader["8"])
+							}
+						};
+						values.Add(carPricingViewModel);
+					}
+				}
+				_context.Database.CloseConnection();
+				return values;
+			}
 		}
 	}
 }
