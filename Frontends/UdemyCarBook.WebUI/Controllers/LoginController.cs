59bcfe95f9309fd10dedf053687cc1809ce14d05
ArdaCimen <cimenarda.yaz@gmail.com>
2024-11-11 13:33:34 +0300

    Register İşlemi Ve Login UI Kısmı

---
+++ Frontends/UdemyCarBook.WebUI/Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using System.Text.Json;
+using System.Text;
+using UdemyCarBook.Dto.LoginDtos;
+using UdemyCarBook.WebUI.Models;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace UdemyCarBook.WebUI.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public LoginController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+      
+    }
+}
